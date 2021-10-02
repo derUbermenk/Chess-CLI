14ec2fcd8f3c3f17e67c921e8c3156fbd5cf68e7
Chester Asuncion <asuncionmcv@gmail.com>
2021-10-02 21:00:12 +0800

    Defining methods for game
    Note:
    	* save_game is not yet defined, also not yet decided on implementation of player move particular
    	  ly the pattern matching part.

--- lib/game.rb
+++ lib/game.rb
@@ -4,5 +4,70 @@
 # ... serves as interface for all relevant chess objects to
 # ... interact with each other
 class Game
+  def initialize
+    @player1 = Player.new
+    @player2 = Player.new
+    @board = Board.new
 
+    @player_que = [@player1, @player2]
+  end
+
+  def play
+    turn_order until end_game
+
+    end_cause
+  end
+
+  # switches between player to allow for moves
+  def turn_order
+    player_turn
+    @board.show
+
+    @player_que.rotate!
+  end
+
+  # gets input for player with regards to saving and
+  # moves. matches input to save game or undo move inputs
+  # else assumes that player entered move input
+  def player_turn
+    player_input = gets.chomp
+    save_game if player_input.match?(ss-<filename>)
+    undo_game if player_input.mathc?(uz)
+    
+    current_player.move(player_input)
+  end
+
+  # checks if an end game condition has been met
+  def end_game
+    current_player.checkmate? || current_player.stalemate?
+  end
+
+  # checks if an end_cause has been done
+  def end_cause
+    if current_player.checkmate?
+      checkmate_message(@player_que.last)
+    elsif current_player.stalemate?
+      stalemate_message
+    end
+  end
+
+  private
+  # saves a game
+  def save_game 
+  end
+
+  # not implemented yet
+  def undo_move; end
+
+  def current_player
+    @player_que.first
+  end
+
+  def checkmate_message(winner)
+    "Checkmate! #{winner.name} wins"
+  end
+
+  def stalemate_message
+    'Stalemate! Draw'
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,25 @@
 # frozen_string_literal: true
+require_relative '../lib/game'
+
+describe Game do
+  describe '#play' do
+    # check internal methods
+  end
+
+  describe '#turn_order' do
+    subject(:game) { described_class.new }
+    it 'rotates player que and allow turn rotation' do; end
+  end
+
+  describe '#end_game?' do
+    it 'checks if end game conditions have been met' do; end
+  end
+
+  describe '#end_cause' do
+    it 'returns a message containing why the game ended' do; end
+  end
+
+  describe '#save' do
+    it 'saves the current state of a game' do; end
+  end
+end
