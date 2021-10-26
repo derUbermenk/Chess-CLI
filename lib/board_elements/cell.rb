dcfb6b0031021c9497fd9baaa4ca4e3cb12f1d64
Chester Asuncion <asuncionmcv@gmail.com>
2021-10-26 20:14:40 +0800

    adding initialize method for a cell object

--- lib/board_elements/cell.rb
+++ lib/board_elements/cell.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-require_relative '../lib/board_elements'
-require_relative '../lib/board'
-
 # cells make up boards. this allows movements of pieces
 # in a board
 class Cell
+  attr_accessor :cell_key, :square, :coordinate
 
-  # @param cell_key [Symbol]
-  # @param square [Square]
   def initialize(cell_key, square)
-    @cell_key = 
-    @square =
-    @
+    @cell_key = cell_key
+    @square = square
+    @coordinate = nil
   end
-end 
+
+
+end
