0c8e5678b02cc0f90015e2b82328a5e47c52ad21
Chester Asuncion <asuncionmcv@gmail.com>
2021-10-28 19:40:55 +0800

    changed cell.piece representation from array to actual piece value

--- lib/board_elements/cell.rb
+++ lib/board_elements/cell.rb
@@ -3,11 +3,24 @@
 # cells make up boards. this allows movements of pieces
 # in a board
 class Cell
-  attr_accessor :cell_key, :square, :coordinate
+  attr_accessor :key, :square, :coordinate,
+                :piece, :from_connections, :to_connections
 
-  def initialize(cell_key, square)
-    @cell_key = cell_key
+  def initialize(key, square)
+    @key = key
     @square = square
+    @piece = nil
     @coordinate = nil
+
+    @from_connections = {}
+    @to_connections = []
+  end
+
+  # removes the piece in self
+  def remove_piece
+    piece = @piece
+    @piece = nil
+
+    piece
   end
 end
