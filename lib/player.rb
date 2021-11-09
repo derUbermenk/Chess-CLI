d30e14e206cd24ef1d9c54a5b9e2878d590e7e82
Chester Asuncion <asuncionmcv@gmail.com>
2021-11-09 10:04:18 +0800

    added valid checks for move inputs in player

--- lib/player.rb
+++ lib/player.rb
@@ -29,23 +29,12 @@ class Player
 
   # checks input validity
   # ... returns null if input is invalid
-  # @param player_move [Hash] piece_key: in_cell: to_cell:
-  def valid(player_move, board_db)
-    in_cell = board_db[player_move[:in_cell].to_sym]
-    to_cell = board_db[player_move[:to_cell].to_sym]
+  # @param move [Hash] piece_key-in_cell: to_cell:
+  def valid(move, allowed_moves)
+    piece = move[:piece]
+    to_cell = move[:to_cell]
 
-    piece_key = player_move[:piece_key]
-    in_cell_piece = in_cell.piece
-
-    not_skewed = !@king.skewed_positions.include?(in_cell.key)
-    correct_piece = valid_piece?(piece_key, in_cell_piece)
-    correct_cell = valid_cell?(in_cell, to_cell)
-
-    valid_move = not_skewed && correct_piece && correct_cell
-
-    return valid_move && removes_check(to_cell) if @king.check?
-
-    valid_move
+    allowed_moves[piece]&.include?(to_cell) || false
   end
 
   # calls functions for updating cell states
@@ -56,38 +45,6 @@ class Player
     to_cell = board_db[player_move[:to_cell]]
 
     in_cell.move_piece_to(to_cell)
-  end
-
-  private
-
-  # checks if to_cell is in positions which can remove a check
-  # @param to_cell [Symbol]
-  def removes_check?(to_cell)
-    @king.check_removers.include?(to_cell)
-  end
-
-  # checks if the cell being accessed by player move contains
-  # ... a piece being referenced by the move and that the piece
-  # ... is of the same color
-  # @param piece_key [Symbol] the player input piece key 
-  # @param in_cell_piece [Cell] the piece that is in in_cell
-  # @return [TrueClass, FalseClass]
-  def valid_piece?(piece_key, in_cell_piece)
-    same_key = in_cell_piece.key == piece_key
-    same_color = in_cell_piece.color == @color
-
-    return true if same_key && same_color
-  end
-
-  # checks if the in_cell and to_cell are connected
-  # and if to_cell contains a piece of opposite color
-  # @param in_cell [Cell]
-  # @param to_cell [Cell]
-  def valid_cell?(in_cell, to_cell)
-    connected = in_cell.to_connections[to_cell.key]
-    capturing_opposite_color = to_cell.piece.color != @color
-
-    return true if connected && capturing_opposite_color
   end
 
   # formats player move in to hash
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,20 +4,37 @@ require_relative '../lib/player'
 
 describe Player do
   describe '#move' do
-    it "updates a chosen piece's position" do; end
   end
 
   describe '#valid' do
-    it 'returns the player_move hash if the move is valid' do; end
-    it 'returns the nil if the move is not valid' do; end
-  end
+    let(:player1) { Player.new(:white, King.new(:white)) }
+    context 'when player is moving a valid piece but not to a valid cell' do
+      it 'returns false' do
+        # validity = allowed_moves[piece] && allowed_moves[piece].include?(to_cell)
+        move = {piece: 'n-d4', to_cell: :a5}
+        allowed_moves = {'q-d3' => %i[d5 d7 d8 a5], 'n-d4' => %i[d9 d10 d11]}
+        validity = player1.valid(move, allowed_moves)
+        expect(validity).to be false
+      end
+    end
 
-  describe '#checkmate?' do
-    it 'checks if players king is in checkmate' do; end
+    context 'when player is trying to move a absent piece' do
+      it 'returns false' do
+        # validity = allowed_moves[piece] && allowed_moves[piece].include?(to_cell)
+        move = { piece: 'n-d3', to_cell: :a5 }
+        allowed_moves = {'q-d3' => %i[d5 d7 d8 a5], 'n-d4' => %i[d9 d10 d11]}
+        validity = player1.valid(move, allowed_moves)
+        expect(validity).to be false
+      end
+    end
 
-  end
-
-  describe '#stalemate' do
-    it 'checks if players king is in stalemate' do; end
+    context 'when player is trying to move a valid piece to a valid cell' do
+      it 'returns true' do
+        move = { piece: 'n-d3', to_cell: :a5 }
+        allowed_moves = { 'n-d3' => %i[d5 d7 d8 a5], 'n-d4' => %i[d9 d10 d11] }
+        validity = player1.valid(move, allowed_moves)
+        expect(validity).to be true
+      end
+    end
   end
 end
